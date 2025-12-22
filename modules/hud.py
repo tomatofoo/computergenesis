7368a0b45bd7f1e2b32ee2e09871a37418a655b9
Tomatofu <99457089+tomatophu@users.noreply.github.com>
2025-12-21 23:29:42 -0800

    add basic hud interface

--- modules/hud.py
+++ modules/hud.py
@@ -2,18 +2,32 @@ from numbers import Real
 from typing import Self
 
 import pygame as pg
+from pygame.typing import Point
 
 
 class HUDElement(object):
     def __init__(self: Self, surf: pg.Surface) -> None:
         self._surf = surf
 
-    def update(self: Self) -> None:
+    @property
+    def surf(self: Self) -> None:
+        return self._surf
+
+    def update(self: Self, rel_game_speed: Real) -> None:
         pass
 
 
 class HUD(object):
-    def __init__(self: Self, ) -> None:
-        pass
+    def __init__(self: Self,
+                 elements: dict[object, list[Point, HUDElement]]]) -> None:
+        self._elements = elements
+
+    def update(self: Self, rel_game_speed: Real) -> None:
+        for _, element in self._elements.values():
+            element.update(rel_game_speed)
+
+    def render(self: Self, surf: pg.Surface) -> None:
+        for pos, element in self._elements.values():
+            surf.blit(element.surf, pos)
 
 

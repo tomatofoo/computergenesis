9655632f0ef7b801e44a5047c1b58f434fb64a47
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-01-09 14:21:21 -0800

    remove HUDWeapon

--- modules/hud.py
+++ modules/hud.py
@@ -36,45 +36,5 @@ class HUD(object):
             surf.blit(element.surf, element.pos)
 
 
-class HUDWeapon(HUDElement):
-    def __init__(self: Self,
-                 default: list[pg.Surface],
-                 attack: list[pg.Surface],
-                 pos: Point,
-                 bob_frequency: Real=0.08335,
-                 bob_strength: Real=20,
-                 default_frame_time: Real=0,
-                 attack_frame_time: Real=15) -> None:
-
-        self._default = default
-        self._attack = attack
-        self._default_frame_time = default_frame_time
-        self._attack_frame_time = attack_frame_time
-        self._surf = default[0]
-
-        self._original_pos = pos
-        self._pos = pos
-
-        self._bob_frequency = bob_frequency
-        self._bob_strength = bob_strength
-
-    @property
-    def surf(self: Self) -> pg.Surface:
-        return self._surf
-
-    @property
-    def pos(self: Self) -> tuple:
-        return self._pos
-
-    def update(self: Self, rel_game_speed: Real, level_timer: Real) -> None:
-        self._pos = (
-            (self._original_pos[0]
-             + math.sin(level_timer * self._bob_frequency)
-             * self._bob_strength),
-            (self._original_pos[1]
-             + abs(math.cos(level_timer * self._bob_frequency))
-             * self._bob_strength / 2),
-        )
-
 # CUSTOM
 

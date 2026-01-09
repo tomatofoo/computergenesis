fbcedfe2681ccf82af44c1871eda02695edc01dc
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-01-09 14:25:02 -0800

    create menu.py

---
+++ modules/menu.py
@@ -0,0 +1,14 @@
+from typing import Self
+
+import pygame as pg
+
+
+class MenuItem(object):
+    def __init__(self: Self) -> None:
+        pass
+
+
+class Menu(object):
+    def __init__(self: Self) -> None:
+        pass
+

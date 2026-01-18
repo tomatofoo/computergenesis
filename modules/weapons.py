7b1cf44d87ed670bfb249a6c7b750879ea72bec0
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-01-18 14:54:50 -0800

    whta the fuck

--- modules/weapons.py
+++ modules/weapons.py
@@ -97,7 +97,8 @@ class AmmoWeapon(Weapon):
             attack_animation_time=attack_animation_time,
             attack_sound=attack_sound,
         )
-
+        
+        self._capacity = capacity
         self._ammo = 0
 
     @property
@@ -239,69 +240,6 @@ class MissileWeapon(AmmoWeapon):
     def speed(self: Self, value: Real) -> None:
         self._speed = value
 
-
-class WeaponItem(object):
-    def __init__(self: Self, weapon: Weapon) -> None:
-        self._weapon = weapon
-    
-    @property
-    def weapon(self: Self) -> Weapon:
-        return self._weapon
-
-    @weapon.setter
-    def weapon(self: Self, value: Weapon) -> None:
-        self._weapon = value
-
-
-class MeleeWeaponItem(WeaponItem):
-    def __init__(self: Self, weapon: Weapon, durability: int) -> None:
-        super().__init__(weapon)
-        self._durability = durability
-
-    @property
-    def durability(self: Self) -> int:
-        return self._durability
-
-    @durability.setter
-    def durability(self: Self, value: int) -> None:
-        self._durability = value
-
-
-class AmmoWeaponItem(WeaponItem):
-    def __init__(self: Self, weapon: Weapon, ammo: int) -> None:
-        super().__init__(weapon)
-        self._ammo = ammo
-
-    @property
-    def ammo(self: Self) -> int:
-        return self._ammo
-
-    @ammo.setter
-    def ammo(self: Self, value: int) -> None:
-        self._ammo = value
-
-
-class WeaponManager(object):
-    def __init__(self: Self, weapons: dict[str, Weapon]={}) -> None:
-        self._weapons = weapons
-
-    @property
-    def weapons(self: Self) -> dict[str, Weapon]:
-        return self._weapons
-
-    @weapons.setter
-    def weapons(self: Self, value: dict[str, Weapon]) -> None:
-        self._weapons = value
-    
-    def set_weapon(self: Self, id: str, weapon: Weapon) -> None:
-        self._weapons[id] = weapon
-    
-    # returns if weapon was added in any way
-    def add_weapon(self: Self, id: str, weapon: Weapon) -> bool:
-        pass
-
-    def pop_weapon(self: Self, id: str) -> Weapon:
-        return self._weapons.pop(id)
 
 # CUSTOM
 

e13e604729c4bd0f16db377114f8e31642e0e4c3
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-01-28 11:38:43 -0800

    add maind menu

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from data.levels import LEVELS
 from modules.camera import Camera
 from modules.hud import HUDElement
 from modules.hud import HUD
-from modules.entities import Player
+from modules.menu import Menu
 from modules.utils import SMALL
 from modules.utils import gen_img_path
 
@@ -53,6 +53,10 @@ class Game(object):
                 'right': pg.K_d,
                 'look_left': pg.K_LEFT,
                 'look_right': pg.K_RIGHT,
+                'menu_up': pg.K_UP,
+                'menu_down': pg.K_DOWN,
+                'menu_enter': pg.K_RETURN,
+                'pause': pg.K_ESCAPE,
             },
         }
         self._screen = pg.display.set_mode(
@@ -65,6 +69,7 @@ class Game(object):
         self._running = 0
         
         pg.mouse.set_relative_mode(1)
+        pg.key.set_repeat(300, 75)
         
         # Level
         self._level = LEVELS[0]
@@ -84,8 +89,43 @@ class Game(object):
         self._camera.horizon = 0.5
         self._camera.camera_offset = 5 / 6 * self._player.height
         self._camera.weapon_scale = 3 / self._SURF_RATIO[0]
+
+        # Menu
+        self._fonts = {
+            'normal': [
+                pg.Font('data/fonts/Pixbob.ttf', 10),
+                pg.Font('data/fonts/Pixbob.ttf', 20),
+            ],
+            'bold': [
+                pg.Font('data/fonts/Pixbob Bold.ttf', 10),
+                pg.Font('data/fonts/Pixbob Bold.ttf', 20),
+            ],
+        }
+        main = Menu(
+            self._fonts['bold'][1],
+            y=32,
+            gap=12,
+            selected_color=(255, 0, 0),
+        )
+        logo = pg.image.load(gen_img_path('logo.png')).convert()
+        logo.set_colorkey((255, 0, 255))
+        main.surf(logo)
+        main.button('Play', self.play)
+        main.button('Settings', self.settings)
+        main.button('Credits', self.credits)
+        main.button('Quit', self.quit)
+        self._menus = {
+            'main': main,
+            'settings': None,
+            'credits': None,
+            'pause': None,
+            'last': None, # last menu selected
+        }
+        # main, credits, settings, pause, playing
+        self._state = 'main'
+        self._menus['main'].selected = 0
         
-        # Misc
+        # Movement
         self._offset_ratio = 5 / 6
         self._player_height = 0.6
         self._crouch_height = 0.3125
@@ -156,6 +196,18 @@ class Game(object):
             height,
         ) * self._slide_time
 
+    def play(self: Self) -> None:
+        self._state = 'playing'
+
+    def settings(self: Self) -> None:
+        self._state = 'settings'
+
+    def credits(self: Self) -> None:
+        self._state = 'credits'
+
+    def quit(self: Self) -> None:
+        self._running = 0
+
     def run(self: Self) -> None:
         self._running = 1
 
@@ -188,119 +240,134 @@ class Game(object):
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self._running = 0
-                elif event.type == pg.MOUSEMOTION:
-                    rel = pg.mouse.get_rel()
-                    self._player.yaw += rel[0] * self._mouse_look_speed
-                    # self._camera.horizon -= rel[1] * 0.0025
-                elif event.type == pg.MOUSEBUTTONDOWN:
-                    self._player.attack()
-                elif event.type == second:
-                    fps = mean(frames)
-                    pg.display.set_caption(str(int(fps)))
-                    frames = []
+                elif self._state == 'playing':
+                    if event.type == pg.MOUSEMOTION:
+                        rel = pg.mouse.get_rel()
+                        self._player.yaw += rel[0] * self._mouse_look_speed
+                        # self._camera.horizon -= rel[1] * 0.0025
+                    elif event.type == pg.MOUSEBUTTONDOWN:
+                        self._player.attack()
+                    elif event.type == second:
+                        fps = mean(frames)
+                        pg.display.set_caption(str(int(fps)))
+                        frames = []
+                    elif event.type == pg.KEYDOWN:
+                        # TEMP
+                        if event.key == pg.K_1:
+                            self._player.weapon = WEAPONS['fist']
+                        elif event.key == pg.K_2:
+                            self._player.weapon = WEAPONS['shotgun']
+                        elif event.key == pg.K_3:
+                            self._player.weapon = WEAPONS['launcher']
+                        elif event.key == pg.K_0:
+                            SOUNDS['water'].play(pos=(9, 0.25, 9)) 
+                        elif event.key == self._settings['keys']['interact']:
+                            self._player.interact()
+                        elif not sliding:
+                            if (event.key == self._settings['keys']['slide']
+                                and jumping):
+                                sliding = SMALL
+                                mult = (
+                                    keys[self._settings['keys']['forward']]
+                                    - keys[self._settings['keys']['backward']]
+                                )
+                                self._player.boost = (
+                                    self._player.forward
+                                    * self._slide_speed
+                                    * mult
+                                )
+                                self._player.elevation_velocity = (
+                                    self._slide_elevation_velocity
+                                )
+                            elif (event.key == self._settings['keys']['crouch']
+                                  and not jumping
+                                  and not crouching):
+                                crouching = SMALL
                 elif event.type == pg.KEYDOWN:
-                    # TEMP
-                    if event.key == pg.K_1:
-                        self._player.weapon = WEAPONS['fist']
-                    elif event.key == pg.K_2:
-                        self._player.weapon = WEAPONS['shotgun']
-                    elif event.key == pg.K_3:
-                        self._player.weapon = WEAPONS['launcher']
-                    elif event.key == pg.K_0:
-                        SOUNDS['water'].play(pos=(9, 0.25, 9)) 
-                    
-                    elif event.key == self._settings['keys']['interact']:
-                        self._player.interact()
-                    elif not sliding:
-                        if (event.key == self._settings['keys']['slide']
-                            and jumping):
-                            sliding = SMALL
-                            self._player.boost = (
-                                self._player.forward
-                                * self._slide_speed
-                                * (keys[self._settings['keys']['forward']]
-                                   - keys[self._settings['keys']['backward']])
-                            )
-                            self._player.elevation_velocity = (
-                                self._slide_elevation_velocity
-                            )
-                        elif (event.key == self._settings['keys']['crouch']
-                              and not jumping
-                              and not crouching):
+                    menu = self._menus[self._state]
+                    if event.key == self._settings['keys']['menu_up']:
+                        menu.selected -= 1
+                    elif event.key == self._settings['keys']['menu_down']:
+                        menu.selected += 1
+                    elif event.key == self._settings['keys']['menu_enter']:
+                        menu.enter()
+            
+            if self._state == 'playing':
+                # Keys
+                keys = pg.key.get_pressed()
+                
+                # Update
+                if self._level is LEVELS[0]:
+                    self.move_tiles(level_timer)
+                
+                # Movement
+                speed = self._walk_speed
+                self._player.friction = self._walk_friction
+
+                # Slide / Crouch
+                if sliding:
+                    sliding = min(sliding + rel_game_speed, self._slide_time)
+                    self._update_slide_height(sliding)
+                    sliding = self._get_sliding(self._player.height)
+                    if sliding >= self._slide_time:
+                        sliding = 0
+                        if not jumping:
                             crouching = SMALL
+                if crouching:
+                    if keys[self._settings['keys']['crouch']]:
+                        crouching = min(
+                            crouching + rel_game_speed, self._crouch_time,
+                        )
+                        speed = self._crouch_speed
+                        self._player.friction = self._crouch_friction
+                        self._update_crouch_height(crouching)
+                    else:
+                        crouching = max(crouching - rel_game_speed, 0)
+                        self._update_crouch_height(crouching)
+                        crouching = self._get_crouching(self._player.height)
+                        if not crouching:
+                            self._player.height = self._player_height
 
-            # Keys
-            keys = pg.key.get_pressed()
-            
-            # Update
-            if self._level is LEVELS[0]:
-                self.move_tiles(level_timer)
-            
-            # Movement
-            speed = self._walk_speed
-            self._player.friction = self._walk_friction
+                self._camera.camera_offset = (
+                    self._offset_ratio * self._player.height
+                )
+                
+                movement = (
+                    (keys[self._settings['keys']['forward']]
+                     - keys[self._settings['keys']['backward']])
+                    * speed, # FORWARD BACKWARD
+                    (keys[self._settings['keys']['right']]
+                     - keys[self._settings['keys']['left']])
+                    * speed, # LEFT RIGHT
+                    (keys[self._settings['keys']['look_right']]
+                     - keys[self._settings['keys']['look_left']])
+                    * self._key_look_speed, # LOOK LEFT RIGHT
+                    (keys[self._settings['keys']['jump']] and not jumping)
+                    * self._jump_velocity, # JUMP
+                )
+                self._player.update(
+                    rel_game_speed,
+                    level_timer,
+                    movement[0],
+                    movement[1],
+                    movement[2],
+                    movement[3] if movement[3] else None,
+                )
 
-            # Slide / Crouch
-            if sliding:
-                sliding = min(sliding + rel_game_speed, self._slide_time)
-                self._update_slide_height(sliding)
-                sliding = self._get_sliding(self._player.height)
-                if sliding >= self._slide_time:
-                    sliding = 0
-                    if not jumping:
-                        crouching = SMALL
-            if crouching:
-                if keys[self._settings['keys']['crouch']]:
-                    crouching = min(
-                        crouching + rel_game_speed, self._crouch_time,
-                    )
-                    speed = self._crouch_speed
-                    self._player.friction = self._crouch_friction
-                    self._update_crouch_height(crouching)
-                else:
-                    crouching = max(crouching - rel_game_speed, 0)
-                    self._update_crouch_height(crouching)
-                    crouching = self._get_crouching(self._player.height)
-                    if not crouching:
-                        self._player.height = self._player_height
+                if keys[self._settings['keys']['jump']]:
+                    jumping = 1
+                if self._player.collisions['e'][0]:
+                    jumping = 0
 
-            self._camera.camera_offset = (
-                self._offset_ratio * self._player.height
-            )
-            
-            movement = (
-                (keys[self._settings['keys']['forward']]
-                 - keys[self._settings['keys']['backward']])
-                * speed, # FORWARD BACKWARD
-                (keys[self._settings['keys']['right']]
-                 - keys[self._settings['keys']['left']])
-                * speed, # LEFT RIGHT
-                (keys[self._settings['keys']['look_right']]
-                 - keys[self._settings['keys']['look_left']])
-                * self._key_look_speed, # LOOK LEFT RIGHT
-                (keys[self._settings['keys']['jump']] and not jumping)
-                * self._jump_velocity, # JUMP
-            )
-            self._player.update(
-                rel_game_speed,
-                level_timer,
-                movement[0],
-                movement[1],
-                movement[2],
-                movement[3] if movement[3] else None,
-            )
+                self._level.update(rel_game_speed, level_timer)
+                frames.append(1 / delta_time if delta_time else math.inf)
 
-            if keys[self._settings['keys']['jump']]:
-                jumping = 1
-            if self._player.collisions['e'][0]:
-                jumping = 0
+                # Render
+                self._camera.render(self._surface)
+                # self._hud.render(self._surface)
+            else:
+                self._menus[self._state].render(self._surface)
 
-            self._level.update(rel_game_speed, level_timer)
-            frames.append(1 / delta_time if delta_time else math.inf)
-
-            # Render
-            self._camera.render(self._surface)
-            # self._hud.render(self._surface)
             resized_surf = pg.transform.scale(self._surface, self._SCREEN_SIZE)
             self._screen.blit(resized_surf, (0, 0))
             pg.display.flip()
--- modules/menu.py
+++ modules/menu.py
@@ -1,5 +1,6 @@
 from typing import Self
 from typing import Optional
+from typing import Callable
 
 import pygame as pg
 from pygame.typing import ColorLike
@@ -20,32 +21,41 @@ class Menu(object): # centered on x
                      font: pg.Font,
                      unselected: ColorLike,
                      selected: ColorLike,
+                     func: Callable,
                      antialiasing: bool=False) -> None:
 
             self._text = text
-            self._surfs = {
-                'unselected': font.render(text, antialiasing, unselected),
-                'selected': font.render(text, antialiasing, selected),
-            }
+            self._surfs = [
+                font.render(text, antialiasing, unselected),
+                font.render(text, antialiasing, selected),
+            ]
             self._selected = 0
+            self._func = func
 
         @property
         def surf(self: Self) -> pg.Surface:
-            return self._selected
-
+            return self._surfs[self._selected]
+    
+    # Expects there to be at least one button
     def __init__(self: Self,
                  font: pg.Font,
                  y: int=0,
+                 gap: int=0,
                  unselected_color: ColorLike=(255, 255, 255),
                  selected_color: ColorLike=(255, 0, 0),
                  background: Optional[pg.Surface]=None) -> None:
         self._widgets = []
+        self._selected = 0
+        self.selected = 0
+        self._selected_dex = 0 # actual index in widgets list
+        self._font = font
         self._y = y
-        self._gap = 0
+        self._gap = gap
         self._colors = {
             'unselected': unselected_color,
             'selected': selected_color,
         }
+        self._background = background
 
     @property
     def gap(self: Self) -> int:
@@ -55,20 +65,58 @@ class Menu(object): # centered on x
     def gap(self: Self, value: int) -> None:
         self._gap = value
 
+    @property
+    def background(self: Self) -> Optional[pg.Surface]:
+        return self._background
+
+    @background.setter
+    def background(self: Self, value: Optional[pg.Surface]) -> None:
+        self._background = value
+    
+    @property
+    def selected(self: Self) -> int:
+        return self._selected
+
+    @selected.setter
+    def selected(self: Self, value: int) -> None:
+        if value < 0:
+            return None
+        # selects (n + 1)th added selectable
+        i = 0
+        for dex, widget in enumerate(self._widgets):
+            if isinstance(widget, self.Button):
+                if i == value:
+                    widget._selected = 1
+                    selected_dex = dex
+                    break
+                i += 1
+        else:
+            return None
+        if selected_dex != self._selected_dex:
+            self._widgets[self._selected_dex]._selected = 0
+        self._selected_dex = selected_dex
+        self._selected = value
+
+    def enter(self: Self) -> None:
+        self._widgets[self._selected_dex]._func()
+
     def surf(self: Self, surf: pg.Surface) -> None:
         self._widgets.append(self.Widget(surf))
 
-    def button(self: Self, text: str) -> None:
+    def button(self: Self, text: str, func: Callable=lambda: 1) -> None:
         self._widgets.append(
             self.Button(
                 text,
                 self._font,
                 self._colors['unselected'],
                 self._colors['selected'],
+                func,
             ),
         )
 
     def render(self: Self, surf: pg.Surface) -> None:
+        if self._background is not None:
+            surf.blit(self._background, (0, 0))
         y = self._y
         for widget in self._widgets:
             surface = widget.surf

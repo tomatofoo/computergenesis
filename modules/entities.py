4094eb6aa5938fb37d9a14f0ffeaa04639449c6a
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-01-28 12:51:21 -0800

    yes

--- modules/entities.py
+++ modules/entities.py
@@ -726,6 +726,140 @@ class Enemy(EntityEx):
         pass
 
 
+class Pathfinder(object): # 4-path pathfinder using A*
+
+    _STRAIGHT_OFFSETS = {
+        (0, 1): 1,
+        (-1, 0): 1,
+        (1, 0): 1,
+        (0, -1): 1,
+    }
+
+    _DIAGONAL_OFFSETS = {
+        (0, 1): 1,
+        (-1, 0): 1,
+        (1, 0): 1,
+        (0, -1): 1,
+    }
+
+    def __init__(self: Self) -> None:
+        self.start = start
+        self.end = end
+        self.tilemap = tilemap
+
+        self._path = []
+        self._g = {}
+        self._h = {}
+        self._parent = {}
+
+    @property
+    def start(self: Self) -> tuple:
+        return self._start
+
+    @start.setter
+    def start(self: Self, value: Point) -> None:
+        self._start = tuple(value)
+
+    @property
+    def end(self: Self) -> tuple:
+        return self._end
+
+    @end.setter
+    def end(self: Self, value: Point) -> None:
+        self._end = tuple(value)
+    
+    @property
+    def tilemap(self: Self) -> dict:
+        return self._tilemap
+
+    @tilemap.setter
+    def tilemap(self: Self, value: dict) -> None:
+        self._tilemap = value
+
+    @property
+    def path(self: Self) -> list:
+        path = list(self._path)
+        path.reverse()
+        return path
+
+    def _manhattan(self: Self, start: Point, end: Point) -> int:
+        return abs(end[0] - start[0]) + abs(end[1] - start[1])
+
+    def _get_g(self: Self, location: Point) -> Number:
+        g = self._g.get(location)
+        if g == None:
+            g = math.inf
+            self._g[location] = g
+        return g
+
+    def _get_h(self: Self, location: Point) -> Number:
+        h = self._h.get(location)
+        if h == None:
+            h = self._manhattan(location, self._end)
+            self._h[location] = h
+        return h
+
+    def _get_parent(self: Self, location: Point) -> Point:
+        parent = self._parent.get(location)
+        if parent == None:
+            parent = self._start
+            self._parent[location] = parent
+        return parent
+
+    def pathfind(self: Self) -> None:
+        if (self._tilemap.get(self._start) != None
+            or self._tilemap.get(self._end) != None):
+
+            return None
+
+        # Setup
+        self._g = {}
+        self._h = {}
+        self._parent = {}
+        visited = set()
+         
+        will = {self._start}
+        self._g[self._start] = 0
+
+        # Algorithm
+        while will:
+            # Find the node
+            least = math.inf
+            for tentative in will:
+                f = self._get_g(tentative) + self._get_h(tentative)
+                if f < least:
+                    node = tentative
+                    least = f
+            will.remove(node)
+            visited.add(node)
+
+            if node == self._end:
+                break
+            
+            # Check Neighbor
+            for offset, weight in self._TILE_OFFSETS.items():
+                neighbor = (node[0] + offset[0], node[1] + offset[1])
+                tentative = self._get_g(node) + weight
+                invalid = (
+                    neighbor in visited
+                    or self._tilemap.get(gen_tile_key(neighbor))
+                    or tentative >= self._get_g(neighbor)
+                )
+                if invalid:
+                    continue
+
+                self._g[neighbor] = tentative
+                self._parent[neighbor] = node
+                will.add(neighbor)
+        
+        # Trace path back
+        self._path = []
+        node = self._get_parent(self._end)
+        while node != self._start:
+            node = self._get_parent(node)
+            self._path.append(node)
+
+
 class Missile(EntityEx):
     def __init__(self: Self,
                  damage: Real,

942cf92cfb3fbb813976e1e1e51eb5bafa058206
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-02-24 16:17:54 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+
 # Setup
 DIR=$PWD
 MODULES=("pathfind" "camera")
 
 # Compile Cython Modules
-for module in $MODULES
-do
+for module in $MODULES; do
     cd $DIR/modules/$module
     python3 setup.py build_ext --build-lib=../
 done
 
-# Final build
-cd $DIR
-python3 setup.py build
+
+# Decide whether or not to build main
+for arg in "$@"; do
+    case $arg in
+        -m|--main)
+        # Final build
+        cd $DIR
+        python3 setup.py build
+        ;;
+    esac
+done
 
